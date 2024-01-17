7441490e44d00437d25cf09e8146fa708bf3aa78
yusixian <cosine_yu@qq.com>
2024-01-17 23:18:49 +0800

    fix: pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  apps : [{
-    name: 'cos-space-api',
-    script: 'pnpm start',
-    instances: 1,
-    watch: true,
-    max_memory_restart: '180M',
-    // env: {
-    //   NODE_ENV: 'production',
-    // }, 
-  }]
-}
+  apps: [
+    {
+      name: 'cos-space-api',
+      script: 'pnpm start',
+      instances: 1,
+    },
+  ],
+};
